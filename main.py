119f6afa04543e50cf89604d7ad1bc1eb9df3ff5
Toni Pizà <servomac@gmail.com>
2017-06-30 10:00:59 +0200

    Obtain the dimensionality from the data

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from keras.layers.core import Dense, Dropout
 
 from data import load_data
 
+def _count_classes(y):
+    return len(set([tuple(category) for category in y]))
+
 X_train, X_test, Y_train, Y_test = load_data()
 
-#(batch_size, timesteps, input_dim)
-timesteps = 128
-input_dim = 9
-output_dim = 6
+timesteps = len(X_train[0])
+input_dim = len(X_train[0][0])
+n_classes = _count_classes(Y_train)
 
 model = Sequential()
 model.add(LSTM(32, input_shape=(timesteps, input_dim)))
 model.add(Dropout(0.5))
-model.add(Dense(output_dim, activation='sigmoid'))
+model.add(Dense(n_classes, activation='sigmoid'))
 
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
